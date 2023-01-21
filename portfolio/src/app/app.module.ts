d149ed73352205d496356b58ca64c8b6d6f82543
alvaroherbon <57678071+alvaroherbon@users.noreply.github.com>
2023-01-21 22:10:34 +0100

    modified toolbar appearence, offcanvas style

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -41,6 +42,7 @@ import { NewsCardListComponent } from './components/news-card-list/news-card-lis
     MatCardModule,
     MatToolbarModule,
     MatIconModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- portfolio/src/app/components/toolbar/toolbar.component.ts
+++ portfolio/src/app/components/toolbar/toolbar.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.css']
+  styleUrls: ['./toolbar.component.css'],
 })
 export class ToolbarComponent {
+  text = 'hola';
 
+  onSubmit() {
+    console.log(this.text);
+  }
 }
