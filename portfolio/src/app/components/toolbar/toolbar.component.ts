7d5d406c1c19c4654ed52d9b39c2c9197ce9d5cb
alvaroherbon <57678071+alvaroherbon@users.noreply.github.com>
2023-01-26 15:26:42 +0100

    modified styles and toolbar

--- portfolio/src/app/components/toolbar/toolbar.component.ts
+++ portfolio/src/app/components/toolbar/toolbar.component.ts
@@ -12,13 +12,4 @@ export class ToolbarComponent {
   onSubmit() {
     console.log(this.text);
   }
-
-  activeTab(event: any) {
-    console.log('el evento es: ', event.target);
-    const navBarItems = document.querySelectorAll('.navbar-nav>li>a');
-    navBarItems.forEach((item) => {
-      item.classList.remove('active');
-    });
-    event.target.classList.add('active');
-  }
 }
